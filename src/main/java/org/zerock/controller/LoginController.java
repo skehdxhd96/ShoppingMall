360127beb729bd5ced758cf948afd30756016b25
EunhaKyeong <eh0634a@gmail.com>
2021-01-22 23:36:54 +0900

    로그인페이지 수정, 로그인 리다이렉트 페이지 수정

--- src/main/java/org/zerock/controller/LoginController.java
+++ src/main/java/org/zerock/controller/LoginController.java
@@ -1,17 +1,52 @@
 package org.zerock.controller;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.CustomerVO;
+import org.zerock.oauth.SocialLogin;
+import org.zerock.oauth.SocialValue;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
 
 @Controller
 public class LoginController {
-	//ë¡œê·¸ì¸ í˜ì´ì§€
+	@Inject
+	private SocialValue naverLogin;
+	
+	@Inject
+	private SocialLogin naver;
+	
+	//·Î±×ÀÎ ÆäÀÌÁö
 	@RequestMapping("/login")
-	public String login() {
+	public String login(Model model, HttpSession session) {
+		naver = new SocialLogin(naverLogin, session);
+		String naverLoginUrl = naver.getAuthorizationUrl();
+	
+		model.addAttribute("naverLoginUrl", naverLoginUrl);
 		
 		return "login";
 	}
-	//íšŒì›ê°€ì… í˜ì´ì§€
+	
+	//¼Ò¼È·Î±×ÀÎ Äİ¹é
+	@RequestMapping(value="/login/{social}/callback", method=RequestMethod.GET)
+	public void loginCallback(Model model, @PathVariable String social, HttpSession session, @RequestParam String state, @RequestParam String code) throws Exception {
+		if (social.equals("naver")) {
+			OAuth2AccessToken accessToken = naver.getAccessToken(code, state, session);
+			CustomerVO customerVO = naver.getProfile(accessToken);
+			
+			System.out.println(customerVO.getCustomerName());
+			
+		}
+	}
+	
+	//È¸¿ø°¡ÀÔ ÆäÀÌÁö
 	@RequestMapping("/signUp")
 	public String signUp() {
 		

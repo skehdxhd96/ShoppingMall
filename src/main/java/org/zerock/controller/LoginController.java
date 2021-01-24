4e1be378f12ea63a399278cbc03a50c9a336ee56
EunhaKyeong <eh0634a@gmail.com>
2021-01-24 23:25:14 +0900

    소셜로그인 인증 후 존재하는 데이터인지 확인하는 로직&회원가입 폼 제출 시 넘어가는 서버 작업 추가

--- src/main/java/org/zerock/controller/LoginController.java
+++ src/main/java/org/zerock/controller/LoginController.java
@@ -1,6 +1,10 @@
 package org.zerock.controller;
 
+import java.net.http.HttpRequest;
+import java.util.Map;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -9,9 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 import org.zerock.domain.CustomerVO;
 import org.zerock.oauth.SocialLogin;
 import org.zerock.oauth.SocialValue;
+import org.zerock.service.CustomerServiceImpl;
 
 import com.github.scribejava.core.model.OAuth2AccessToken;
 
@@ -22,6 +29,9 @@ public class LoginController {
 	
 	@Inject
 	private SocialLogin naver;
+	
+	@Inject
+	private CustomerServiceImpl customerService;
 	
 	//로그인 페이지
 	@RequestMapping("/login")
@@ -36,19 +46,44 @@ public class LoginController {
 	
 	//소셜로그인 콜백
 	@RequestMapping(value="/login/{social}/callback", method=RequestMethod.GET)
-	public void loginCallback(Model model, @PathVariable String social, HttpSession session, @RequestParam String state, @RequestParam String code) throws Exception {
+	public String loginCallback(Model model, @PathVariable String social, HttpSession session, 
+			@RequestParam String state, @RequestParam String code, RedirectAttributes redirectAttributes) throws Exception {
+		CustomerVO loginCustomer = null;
+		
 		if (social.equals("naver")) {
 			OAuth2AccessToken accessToken = naver.getAccessToken(code, state, session);
-			CustomerVO customerVO = naver.getProfile(accessToken);
-			
-			System.out.println(customerVO.getCustomerName());
-			
+			loginCustomer = naver.getProfile(accessToken);
 		}
+		
+		//유저 존재 여부 확인
+		CustomerVO checkCustomer = customerService.isCustomer(loginCustomer.getSocialId());
+		if (checkCustomer==null) {
+			System.out.println("회원가입 페이지로 이동합니다.");
+			redirectAttributes.addFlashAttribute("newCustomer", loginCustomer);
+			
+			return "redirect:/signUp";
+		}
+		else {
+			System.out.println("존재하는 정보입니다.");
+		}
+		return "/";
 	}
 	
 	//회원가입 페이지
-	@RequestMapping("/signUp")
-	public String signUp() {
+	@RequestMapping(value="/signUp", method=RequestMethod.GET)
+	public String signUpGet(Model model, HttpServletRequest request) {
+		Map<String, ?> flashMap = RequestContextUtils.getInputFlashMap(request);
+		
+		if (flashMap!=null) {
+			CustomerVO newCustomer = (CustomerVO) flashMap.get("newCustomer");
+			model.addAttribute("newCustomer", newCustomer);
+		}
+		
+		return "signUp";
+	}
+	
+	@RequestMapping(value="/signUp", method=RequestMethod.POST) 
+	public String signUpPost() {
 		
 		return "signUp";
 	}

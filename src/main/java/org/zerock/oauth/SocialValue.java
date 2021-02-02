2901e6facd22806158b9fdff3d79582eb3400f1c
EunhaKyeong <eh0634a@gmail.com>
2021-02-02 22:53:03 +0900

    deleteTokenUrl 필드 추가

--- src/main/java/org/zerock/oauth/SocialValue.java
+++ src/main/java/org/zerock/oauth/SocialValue.java
@@ -13,6 +13,7 @@ public class SocialValue {
 	private String reauthCallbackUrl;
 	private DefaultApi20 api20Instance;
 	private String profileUrl;
+	private String deleteTokenUrl;
 	
 	public SocialValue(String socialType, String clientId, String clientSecret, String callbackUrl, String reauthCallbackUrl) {
 		this.socialType = socialType;
@@ -24,6 +25,7 @@ public class SocialValue {
 		if (socialType.equals("naver")) {
 			this.api20Instance = NaverAPI20.instance();
 			this.profileUrl = "https://openapi.naver.com/v1/nid/me";
+			this.deleteTokenUrl = "https://nid.naver.com/oauth2.0/token?grant_type=delete";
 		}
 	}
 }

2dfcdffce3dfa32defe552785ce55c970122cde0
EunhaKyeong <eh0634a@gmail.com>
2021-02-04 12:52:37 +0900

    회원가입 유효성 검사 함수를 회원정보 수정 시 유효성 검사에도 적용하기 위해 다른 파일로 이동

--- src/main/webapp/resources/signUp/js/signUp.js
+++ src/main/webapp/resources/signUp/js/signUp.js
@@ -46,35 +46,3 @@ $("select[name=customerType]").change(function() {
 	
 	$(".addinput").append(formHtml);
 });
-
-//회원가입 submit 전 유효성 검사하는 함수
-$(".signupForm").submit(function() {
-	var formControls = $(".form-control");
-	var result = true;
-	
-	//제출 시 null값이 존재하는지 확인하는 유효성 검사
-	for (var i=0; i<formControls.length; i++) {
-		console.log(formControls[i].value);
-		if (formControls[i].value=="") {
-			alert("모든 값이 입력되지 않았습니다.");
-			formControls[i].focus();
-			result = false;
-			break;
-		}
-	}
-	
-	//전화번호 형식 유효성 검사
-	var regPhone = /^\d{3}-\d{3,4}-\d{4}$/;
-	var inputPhone = $(".form-control[name=customerPhone]");
-	if (!regPhone.test(inputPhone.val())) {
-		alert("휴대전화 형식을 맞춰주세요");
-		inputPhone.focus();
-		result = false;
-	}
-
-	if (result==true) {
-		alert("회원가입이 완료되었습니다!");
-	}
-	
-	return result;
-});

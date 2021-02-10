9cffe831d9ef1171d0650ac965509e1f1fc9c36a
EunhaKyeong <eh0634a@gmail.com>
2021-02-11 01:41:13 +0900

    요청이 성공이었을 때 배송페이지로 이동하는 코드 작성

--- src/main/webapp/resources/ProductDetail/js/order.js
+++ src/main/webapp/resources/ProductDetail/js/order.js
@@ -1,7 +1,8 @@
 $("#Order_Btn").on('click', function() {
 	var productCode = $('input[name=product_code]').val();
 	var productQuantity = $('input[name=product_quantity]').val();
-	var productPrice = $("h4#productPrice").text().slice(0, -1);;
+	var productPrice = $("h4#productPrice").text().slice(0, -1);
+	var totalPrice = productPrice*productQuantity;
 	
 	axios.post("/order/delivery", {
 		"history" : "detail", 
@@ -10,17 +11,20 @@ $("#Order_Btn").on('click', function() {
 			 "productQuantity" : productQuantity, 
 			 "productPrice" : productPrice
 			}
-		]
+		], 
+		"totalPrice" : totalPrice
 	}).then(function(response) {
-		console.log(response);
-		if (response.data==0) {
+		console.log(response.data);
+		if (response.data.result==0) {
 			alert("로그인이 필요합니다.");
 		}
-		else if (response.data==2) {
+		else if (response.data.result==2) {
 			alert("접근 권한이 없습니다.");
 		}
 		else {
+			var url="/order/delivery/form?deliveryCode=" + response.data.deliveryCode;
 			alert("주문이 완료되었습니다.");
+			location.href = url;
 		}
 	})
 })

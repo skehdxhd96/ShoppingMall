fe6f31693870bfe758065126c141056fd372ae17
EunhaKyeong <eh0634a@gmail.com>
2021-02-12 00:52:56 +0900

    주문상세 테이블 관련 쿼리문 작성

---
+++ src/main/java/org/zerock/mapper/OrderDetailMapper.java
@@ -0,0 +1,7 @@
+package org.zerock.mapper;
+
+import org.zerock.domain.OrderDetailVO;
+
+public interface OrderDetailMapper {
+	public void createOrderDetail(OrderDetailVO odVO);
+}
---
+++ src/main/java/org/zerock/service/OrderDetailServiceImpl.java
@@ -0,0 +1,21 @@
+package org.zerock.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Repository;
+import org.zerock.domain.OrderDetailVO;
+import org.zerock.mapper.OrderDetailMapper;
+
+@Repository
+public class OrderDetailServiceImpl implements OrderDetailService {
+	@Resource
+	OrderDetailMapper odMapper;
+	
+	@Override
+	public void createOrderDetail(int orderCode, int productCode, int productQuantity) {
+		OrderDetailVO odVO = new OrderDetailVO(orderCode, productCode, productQuantity);
+		odMapper.createOrderDetail(odVO);
+		System.out.println("orderDetail 데이터 적재 완료\n" + odVO.toString());
+	}
+
+}

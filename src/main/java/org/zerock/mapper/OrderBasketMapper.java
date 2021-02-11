aa2cdd256ef63085a23cfab791e4a527bbb50bbe
EunhaKyeong <eh0634a@gmail.com>
2021-02-12 00:48:26 +0900

    OrderBasket 테이블 관련 파일 삭제

--- src/main/java/org/zerock/mapper/OrderBasketMapper.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.mapper;
-
-import org.zerock.domain.OrderBasketVO;
-
-public interface OrderBasketMapper {
-	public void createOrderBasket(OrderBasketVO orderBasketVO);
-}
--- src/main/java/org/zerock/service/OrderBasketService.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.service;
-
-import org.zerock.domain.OrderBasketVO;
-
-public interface OrderBasketService {
-	public void createOrderBasket(OrderBasketVO orderBasketVO);
-}

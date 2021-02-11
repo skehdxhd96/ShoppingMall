197d96881568dedd950d5be2b956b3e1089c5bcd
EunhaKyeong <eh0634a@gmail.com>
2021-02-12 00:49:37 +0900

    order 테이블 관련 설정 추가

--- src/main/java/org/zerock/mapper/OrderMapper.java
+++ src/main/java/org/zerock/mapper/OrderMapper.java
@@ -4,4 +4,6 @@ import org.zerock.domain.OrderVO;
 
 public interface OrderMapper {
 	public int createOrder(OrderVO orderVO);
+	//delivery 인터셉터에서 구매자일 때 로그인된 사용자의 배달코드인지 알아보기 위한 쿼리문
+	public long getCustomerCode(int deliveryCode);
 }
--- src/main/java/org/zerock/service/OrderService.java
+++ src/main/java/org/zerock/service/OrderService.java
@@ -1,7 +1,7 @@
 package org.zerock.service;
 
-import org.zerock.domain.OrderVO;
-
 public interface OrderService {
-	public int createOrder(OrderVO orderVO);
+	public int createOrder(int totalPrice, long customerCode);
+	//delivery 인터셉터에서 구매자일 때 로그인된 사용자의 배달코드인지 알아보기 위한 쿼리문
+	public long getCustomerCode(int deliveryCode);
 }

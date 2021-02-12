e0e038458ab29439a144a9a3a8b28fcaec012079
EunhaKyeong <eh0634a@gmail.com>
2021-02-12 23:32:50 +0900

    order 테이블 관련 쿼리문 작성

--- src/main/java/org/zerock/service/OrderServiceImpl.java
+++ src/main/java/org/zerock/service/OrderServiceImpl.java
@@ -26,10 +26,22 @@ public class OrderServiceImpl implements OrderService {
 
 	//delivery 인터셉터에서 구매자일 때 로그인된 사용자의 배달코드인지 알아보기 위한 쿼리문
 	@Override
-	public long getCustomerCode(int deliveryCode) {
-		long customerCode = orderMapper.getCustomerCode(deliveryCode);
+	public long getCustomerCodeByDeliery(int deliveryCode) {
+		long customerCode = orderMapper.getCustomerCodeByDeliery(deliveryCode);
 		
 		return customerCode;
+	}
+
+	@Override
+	public int updateStatus(int orderCode) {
+	
+		return orderMapper.updateStatus(orderCode);
+	}
+
+	@Override
+	public long getCustomerCodeByOrder(int orderCode) {
+		
+		return orderMapper.getCustomerCodeByOrder(orderCode);
 	}
 
 }

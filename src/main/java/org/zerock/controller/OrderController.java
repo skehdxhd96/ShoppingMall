4d2a42db87f60d299ff38cfa956e1d1850d10961
EunhaKyeong <eh0634a@gmail.com>
2021-02-12 23:30:18 +0900

    상품 상세정보 페이지에서 바로 주문하기->주문완료까지 구현 완료

--- src/main/java/org/zerock/controller/OrderController.java
+++ src/main/java/org/zerock/controller/OrderController.java
@@ -1,22 +1,30 @@
 package org.zerock.controller;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 import org.zerock.domain.DeliveryVO;
 import org.zerock.domain.basketVO;
+import org.zerock.mapper.OrderMapper;
 import org.zerock.service.CustomerServiceImpl;
 import org.zerock.service.DeliveryServiceImpl;
 import org.zerock.service.OrderDetailServiceImpl;
+import org.zerock.service.OrderService;
 import org.zerock.service.OrderServiceImpl;
 import org.zerock.service.basketServiceImpl;
 
@@ -99,16 +107,68 @@ public class OrderController {
 		return gson.toJson(resjson);
 	}
 
+	//배송지 입력페이지(GET)
 	@RequestMapping(value="/order/delivery/form", method=RequestMethod.GET)
-	public String orderInput(@RequestParam int deliveryCode, Model model, HttpSession session) {
+	public String deliveryFormGet(@RequestParam int deliveryCode, Model model, HttpSession session) {
 		System.out.println("\n=====================================================\n여기는 배송지입력 페이지");
 		HashMap<String, Object> deliveryHm = deliveryService.getDelivery(deliveryCode);
+		deliveryHm.put("deliveryCode", deliveryCode);
 		HashMap<String, Object> customerHm = customerService.getBuyerProfile((long) session.getAttribute("customerCode"));
 		
 		model.addAttribute("buyer", customerHm);
 		model.addAttribute("recipient", deliveryHm);
 		System.out.println(deliveryHm.toString());
 		System.out.println("=====================================================");
+		
 		return "order/deliveryForm";
+	}
+	
+	//배송지 입력페이지(PATCH)
+	@RequestMapping(value="/order/delivery/form", method=RequestMethod.PATCH)
+	@ResponseBody
+	public String deliveryFormPATCH(@RequestBody DeliveryVO deliveryVO) {
+		System.out.println("\n=====================================================\n배송지 입력이 끝났습니다.");
+		System.out.println(deliveryVO.toString());
+		
+		HashMap<String, Object> resultHm = deliveryService.orderSuccess(deliveryVO);	//배송 테이블 업데이트(result:업데이트 결과, orderCode:업데이트된 데이터의 주문코드 리턴)
+		
+		System.out.println("=====================================================");
+		
+		return gson.toJson(resultHm);
+	}
+	
+	//배송테이블 업데이트 이후 orderStatus=done, basket 데이터 삭제
+	@RequestMapping(value="/order/delivery/after", method=RequestMethod.GET)
+	public String orderSuccess(@RequestParam int orderCode, HttpSession session) {
+		int result = 1;
+		
+		while(result!=0) {	//order 테이블 업데이트, basket 테이블 데이터 삭제 과정 중 오류 발생 체크용
+			List<Integer> productCodes = odService.getProductCodes(orderCode);	//해당 주문코드에 대한 상품코드 리스트 가져오기
+			result = productCodes.size(); //list의 크기가 0이면 해당 상품이 없는것이므로 오류 발생
+			System.out.println("productCodes size:" + result);
+			result = basketService.deleteBasket((long)session.getAttribute("customerCode"), productCodes);	//세션에 저장된 customerCode와 상품코드 리스트에 해당되는 장바구니 데이터 삭제
+			System.out.println("basket 테이블 데이터 삭제 완료");
+			result = orderServie.updateStatus(orderCode);	//해당 orderCode의 orderStatus=done으로 업데이트
+			System.out.println("order 테이블의 orderStatus가 done으로 변경되었습니다. orderCode : " + orderCode);
+			
+			break;
+		}
+		//result가 0(실패)이면 orderError 페이지로, 성공이면 orderSuccess 페이지로 리다이렉트
+		return result==0? "redirect:/order/orderError" : "redirect:/order/orderSuccess?orderCode="+orderCode;
+	}
+	
+	//주문 성공 페이지
+	@RequestMapping(value="/order/orderSuccess", method=RequestMethod.GET) 
+	public String orderSuccess(@RequestParam int orderCode, Model model) {
+		model.addAttribute("orderCode", orderCode);
+		
+		return "/order/orderSuccess";
+	}
+	
+	//주문 실패 페이지
+	@RequestMapping("/order/orderError")
+	public String orderError() {
+		
+		return "/order/orderError";
 	}
 }

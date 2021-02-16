8d4b8f3556e2759a5147c15d285ad708cea0ea07
EunhaKyeong <eh0634a@gmail.com>
2021-02-16 22:33:44 +0900

    서버로 전달할 데이터 중 deliveryCode -> orderCode로 변경

--- src/main/webapp/resources/order/js/deliveryForm.js
+++ src/main/webapp/resources/order/js/deliveryForm.js
@@ -1,13 +1,13 @@
 $(".submit-button").on("click", function() {
 	var data= {
-		"deliveryCode" : $("input[name=deliveryCode]").val(), 
+		"orderCode" : $("input[name=orderCode]").val(), 
 		"recipient" : $("input[name=recipient]").val(), 
 		"shippingAddress" : $("input[name=shippingAddress]").val(), 
 		"deliverPhone" : $("input[name=deliverPhone]").val(),
 		"requests" : $("input[name=requests]").val()
 	}
 	
-	axios.patch("/order/delivery/form", data).then(function(response) {
+	axios.post("/order/delivery/form", data).then(function(response) {
 		if (response.data.result==0||response.data.orderCode==undefined) {	//업데이트를 실패하면
 			alert("배송 업데이트 과정 중 오류가 발생했습니다.");
 			location.href = "/order/orderError";	//주문 오류 발생 안내 페이지로

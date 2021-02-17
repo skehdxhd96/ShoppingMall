17d0aa9c2c2e26e16ec1f8f3c4d4f08d08e9f6a4
EunhaKyeong <eh0634a@gmail.com>
2021-02-17 21:22:13 +0900

    orderCode 가지고 배송지 관련 데이터 가져오는 쿼리문 작성

--- src/main/java/org/zerock/mapper/DeliveryMapper.java
+++ src/main/java/org/zerock/mapper/DeliveryMapper.java
@@ -7,5 +7,7 @@ import org.zerock.domain.DeliveryVO;
 public interface DeliveryMapper {
 	public int createDelivery(DeliveryVO delivery);
 	public HashMap<String, Object> getDelivery(int deliveryCode);
+	public DeliveryVO getDeliveryByOrderCode(int orderCode);
 	public int orderSuccess(DeliveryVO deliveryVO);
+	
 }
